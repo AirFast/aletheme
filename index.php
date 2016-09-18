bdbc9b7431780faef08c68b944f2138db32a10a4
Andrew <AirFast.88@gmail.com>
2016-09-18 22:31:33 +0300

    var

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-
-phpinfo();
-
+$a = 4 * 4;
+define('PI', 3.14);
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Приклад</title>
+</head>
+<body>
+	<h1>Hello, world!</h1>
+	<p><?php echo $a . '<br>' . PI; ?></p>
+</body>
+</html>
