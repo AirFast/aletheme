6cfda96f28f6162a769a3a3b6f52578345420a6f
Andrew <AirFast.88@gmail.com>
2016-09-24 00:07:59 +0300

    .idea

--- index.php
+++ index.php
@@ -1,7 +1,24 @@
 <?php
-$a = 4 * 4;
-define('PI', 3.14);
+$news = [
+	1 => [
+		'title' => 'Урок 1',
+		'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec feugiat malesuada tempus. Mauris maximus vestibulum orci ut pharetra. Etiam in justo id sapien sollicitudin pulvinar dictum sit amet lorem. Nullam quis orci euismod, accumsan felis at, pulvinar lorem. Nulla interdum massa vel tellus tincidunt dignissim. Donec finibus, orci quis interdum condimentum, enim massa interdum purus, ac tincidunt magna massa ac ipsum.'
+	],
+	2 => [
+		'title' => 'Урок 2',
+		'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec feugiat malesuada tempus. Mauris maximus vestibulum orci ut pharetra.'
+	],
+	3 => [
+		'title' => 'Урок 3',
+		'text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec feugiat malesuada tempus. Mauris maximus vestibulum orci ut pharetra. Nulla interdum massa vel tellus tincidunt dignissim. Donec finibus, orci quis interdum condimentum, enim massa interdum purus, ac tincidunt magna massa ac ipsum.'
+	],
+];
+
+
+$id = $_GET['id'];
+$article = $news[$id];
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,6 +27,7 @@ define('PI', 3.14);
 </head>
 <body>
 	<h1>Hello, world!</h1>
-	<p><?php echo $a . '<br>' . PI; ?></p>
+  <h2><?php $article['title']; ?></h2>
+	<p><?php echo $article['text']; ?></p>
 </body>
 </html>
